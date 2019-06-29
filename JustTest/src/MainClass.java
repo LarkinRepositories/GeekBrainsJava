73b0015f8020d90c8651babbad2cfbcd52ae34e9
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-06-29 18:26:12 +0700

    added first test file to my new repo added some more random code

--- JustTest/src/MainClass.java
+++ JustTest/src/MainClass.java
@@ -1,7 +1,20 @@
 public class MainClass {
+
     public static void main(String[] args) {
+        char[][] chars = new char[3][3];
         for(int i = 0; i < (int) Math.random()*5; i++ ) {
             System.out.println("Just test");
         }
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars.length; j++) {
+                chars[i][j] = 'K';
+            }
+        }
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars.length; j++) {
+                System.out.println(chars[i][j]);
+            }
+        }
     }
+
 }
